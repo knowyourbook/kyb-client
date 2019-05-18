9fe4892e6c4a1d149372974c6dfa1a99a3fe4b72
ykrueng <yasirah.krueng@gmail.com>
2019-05-18 07:25:53 -0500

    refactor Home component

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,6 @@ import { connect } from 'react-redux'
 
 import Home from './components/Home/Home'
 import Auth from './components/Auth/Auth'
-import Admin from './components/Admin/Setting'
-import Student from './components/Student/Setting'
-import useAuth from './components/Auth/useAuth'
 import { getUser } from './store/actions/index'
 
 function App(props) {
@@ -18,10 +15,8 @@ function App(props) {
 
   return (
     <div>
-      <Route exact path="/" component={Home} />
+      <Route path="/" component={Home} />
       <Route exact path="/auth" component={Auth} />
-      <Route path="/admin" component={useAuth(Admin)} />
-      <Route path="/student" component={useAuth(Student)} />
     </div>
   )
 }
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Route } from 'react-router-dom'
 
 import { logout } from '../../store/actions'
+import Admin from '../Admin/Setting'
+import Student from '../Student/Setting'
+import useAuth from '../Auth/useAuth'
 
-function Home({ isLoggedIn, logout }) {
+function Home({ isLoggedIn, logout, match }) {
   return (
     <div>
       <h1>Welcome to Know Your Book</h1>
       {isLoggedIn && <button onClick={logout}>logout</button>}
+      <Route exact path={`${match.url}admin`} component={useAuth(Admin)} />
+      <Route exact path={`${match.url}student`} component={useAuth(Student)} />
     </div>
   )
 }
